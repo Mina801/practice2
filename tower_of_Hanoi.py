7f7e89364482edb4f145ca5532a6441f7258a090
{Mina} <{minakhamesi@gmail.com}>
2019-07-28 04:35:36 +0430

    game is working.looks fine

--- tower_of_Hanoi.py
+++ tower_of_Hanoi.py
@@ -1,53 +1,58 @@
 class Stack:
-    def __init__(self,name , limit, size = 0):
+    def __init__(self,name , limit,size = 0 ):
         self.list=[]
         self.name = name
         self.size = size
         self.limit = limit
-        self.last_item = list[-1]
 
     def peek(self):
-        return self.last_item
+        if self.size == 0:
+            return 0
+        else:
+            return self.list[-1]
+
     def push_list(self,disk):
-        self.push_list.append(desk.value)
+        self.push_list.append(disk)
 
     def pull_list(self):
         disk_to_remove = self.list.pop()
 
 
-class Node:
-    def __init__(self,value,next_node=None):
-        self.value = value
 
 print("Let's Play Towers of Hanooi")
 while True:
-    num_of_disk = inpute(print("How many disks do you like to play with?"))
+    num_of_disk =int( input(print("How many disks do you like to play with?")))
+
     if num_of_disk >= 3:
         break
     else:
-        print("please enter a number greater than or equall to 3")
+        print("""
+        please enter a number greater than or equall to 3
+        """)
 stacks=[]
 left_stick = Stack("Left" , num_of_disk , num_of_disk)
-stacks.append(left_stick.name)
-right_stick = Stack("Right", num_of_disk )
-stacks.append(right_stick.name)
+stacks.append(left_stick)
 middle_stick = Stack("Middle" , num_of_disk)
-stacks.append(middle_stick.name)
+stacks.append(middle_stick)
+right_stick = Stack("Right", num_of_disk )
+stacks.append(right_stick)
 
 letters=[]
 for i in stacks:
-    letters.append(i[0])
+    letters.append(i.name[0])
 
-for i in range(num_of_disk:0 , -1):
-    disk=Node(i)
-    left_stick.list.append(disk.value)
+for i in range(num_of_disk ,0 , -1):
+    left_stick.list.append(i)
+
 def get_input():
     while True:
         for i in range(len(stacks)):
-            print("please enter {0} for {1}.".format(letters[i] , stacks[i]))
-        stack_to_change = input()
-        return stack_to_change
-        if stack_to_change in letters:
+            print("please enter {0} for {1}.".format(letters[i] , stacks[i].name))
+        letter = input().upper()
+        if letter in letters:
+            for i in range(len(letters)):
+              if letters[i] == letter:
+                return stacks[i]
             break
         else:
             print("please enter a valid letter.")
@@ -55,17 +60,39 @@ def get_input():
 num_moves = 0
 while True:
     while True:
+        print("---------current stacks-------")
+        for i in stacks:
+            print(i.list)
         print("which stack do you want to move from?")
         stack_to_remove = get_input()
         print("which stack do you want to move to?")
         stack_to_go = get_input()
-        if stack_to_go.peek().value > stack_to_remove.peek().value:
-            break
+        if (stack_to_remove.size > 0) and (stack_to_go.size <=stack_to_go.limit):
+            if stack_to_go.size == 0 :
+                number_to_remove = stack_to_remove.list.pop()
+                stack_to_go.list.append(number_to_remove)
+                stack_to_go.size +=1
+                stack_to_remove.size -=1
+                num_moves +=1
+                break
+            elif stack_to_go.list[-1] > stack_to_remove.list[-1]:
+                number_to_remove = stack_to_remove.list.pop()
+                stack_to_go.list.append(number_to_remove)
+                stack_to_go.size +=1
+                stack_to_remove.size -=1
+                num_moves +=1
+                break
+            else:
+                print("""
+
+            The value you wanna add should be less than the value on the stick.
+
+
+            """)
         else:
-            pint("The value you wnat to add should be less than the value on the stick.")
-        disk_to_change = stack_to_remove.list.pop()
-        stack_to_go.list.append(disk_to_change)
-        num_moves +=1
-    if rihgt_stick.size ==right_stick.limit:
+            print("""
+              Be careful to select the sticks that have values on them.
+              """)
+    if right_stick.size == right_stick.limit:
         print("Congradulation! you won in {0} moves".format(num_moves))
         break
